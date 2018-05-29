01c3c692ca3579a1821e4e6cceba287d5ac3691d
kartikvips <kartikvips@gmail.com>
2018-05-28 22:54:31 -0700

    hw

--- w6d4/webpack.config.js
+++ w6d4/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
       path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
       filename: "bundle.js"
-  }
+  },
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
